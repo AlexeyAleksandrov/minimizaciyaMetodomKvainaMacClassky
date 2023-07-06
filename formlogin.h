8a2803782ed1c96de5470e8047057f7d6b41b52c
AlexeyAleksandrov <chinok73@yandex.ru>
2023-07-06 13:09:35 +0300

    Добавлен define для SPLITTER

--- formlogin.h
+++ formlogin.h
@@ -15,6 +15,14 @@
 #define FILE_STUDENTS_NAME "students.mirea"
 //#define FILE_STUDENT_WORK "mmkwork.mirea"
 
+// автоматический define сплиттера, в зависимости от версии компилятора
+// если не произошла проблема, исправь версию. Чтобы узнать версию используй QString::number(QT_VERSION, 16)
+#if QT_VERSION >= 0x050f00 // версия Qt 5.15.0
+#define SPLITTER Qt::SplitBehavior(Qt::SkipEmptyParts)
+#else
+#define SPLITTER QString::SkipEmptyParts
+#endif
+
 namespace Ui {
 class FormLogin;
 }
